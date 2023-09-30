942f8ce9b1ab06c33086aa1e3d6c3db107a4b2e0
adiw <121597581+diws1@users.noreply.github.com>
2023-09-30 23:24:45 +0700

    Create volume.sh

---
+++ volume.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# simple scripts for volume (with pipewire)
+# created by diws
+
+Icon=""
+Volume=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{ print $2*100"%" }')
+
+echo "$Icon $Volume"
